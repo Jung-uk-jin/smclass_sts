e5295de8c910a43c5dd36558eba316d9d6dbb296
Jung-uk-jin <jungukjin32@gmail.com>
2025-02-05 18:32:43 +0900

    0205 스프링 커밋

---
+++ s0205_05/src/main/java/com/java/Controller/FController.java
@@ -0,0 +1,30 @@
+package com.java.Controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
+
+
+@Controller
+public class FController {
+	@Autowired
+	HttpSession session;
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		return "logout";
+	}
+	
+}
